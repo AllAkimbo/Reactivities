aa1c12f8e1c42d8ee654262ec24852ce5f27b592
Mark <mark.soos@barsquared.com>
2021-03-04 08:31:43 +0000

    End of section 2

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    // controller atributes
+    [ApiController]
+    // controller here is a placeholder for the controller that gets passed
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
